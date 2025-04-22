122e992cf6dba40ac525b297536165dfe76dfddc
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2025-04-22 18:29:03 +0900

    update

---
+++ src/main/java/com/ljw/syncpay/SyncpayApplication.java
@@ -0,0 +1,13 @@
+package com.ljw.syncpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljw/syncpay/SyncpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.syncpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
